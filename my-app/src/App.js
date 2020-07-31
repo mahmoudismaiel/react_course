fffe06f3de7f0edefb439acf7252d54d45ec6e46
mahmoud.ismaiel <mahmoud.ismaiel@gmail.com>
2020-07-31 10:10:53 +0400

    finished till understanding props and props.children usage in Function components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -8,6 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p> This is really working!!</p>
+        <Person name="Rose" age="35"/>
+        <Person name="Roaa" age="7.5">My Hobbies: Reading</Person>
+        <Person name="Rital" age="1"/>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'I\'m React App!!!'));
