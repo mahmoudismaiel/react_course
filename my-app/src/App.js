a77a3f9acdbc18e226765a9076cfe0992553370b
mahmoud.ismaiel <mahmoud.ismaiel@gmail.com>
2020-07-31 10:54:36 +0400

    finished using state and setState as App.js is class-based component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,14 +4,40 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+ state = {
+
+  persons : [
+
+    { name:"Rose", age:35},
+    { name:"Roaa", age:7},
+    { name:"Rital", age:1}
+  ],
+  otherState : "some Other Value"
+ }
+
+ switchNameHandler = () => {
+  //console.log('was Clicked');
+  //window.alert('was Clicked');
+  // DO NOT USE THIS:  this.state.persons[0].name = "Rose Mohamed";
+  this.setState({persons : [
+
+    { name:"Rose Mohamed", age:35},
+    { name:"Roaa", age:7},
+    { name:"Rital", age:0.75}
+  ]
+});
+
+ }
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p> This is really working!!</p>
-        <Person name="Rose" age="35"/>
-        <Person name="Roaa" age="7.5">My Hobbies: Reading</Person>
-        <Person name="Rital" age="1"/>
+        <button onClick={this.switchNameHandler}>switch Person</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbie: reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'I\'m React App!!!'));
